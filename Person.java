00c024da5ecf4397dfac3f67a79ed4eb34799f8f
Viswa <160086723+Viswa-Balan@users.noreply.github.com>
2024-03-05 21:17:29 +0530

    PanCard.java

---
+++ Person.java
@@ -0,0 +1,42 @@
+package org.hibernat.jpaonetouni;
+
+import javax.persistence.*;
+
+@Entity
+public class Person 
+{
+ @Id 
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private int id;
+ @Column(nullable=false)
+ private String name;
+ @Column(nullable=false ,unique=true)
+ private long phone;
+ @OneToOne
+ private PanCard card;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public long getPhone() {
+	return phone;
+}
+public void setPhone(long phone) {
+	this.phone = phone;
+}
+public PanCard getCard() {
+	return card;
+}
+public void setCard(PanCard card) {
+	this.card = card;
+}
+ 
+}
